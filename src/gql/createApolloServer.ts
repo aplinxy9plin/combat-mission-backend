ff0e293b3557b70f7e1d841135986e1b33a82036
yungKillaGit <59793907+yungKillaGit@users.noreply.github.com>
2020-04-18 12:29:49 +0300

    add resolvers from separate modules

--- src/gql/createApolloServer.ts
+++ src/gql/createApolloServer.ts
@@ -1,11 +1,11 @@
 import {ApolloServer} from 'apollo-server-express';
+import {and} from 'apollo-resolvers';
 import {
   schema,
   Query,
   Mutation,
   User as GqlUser,
 } from "combat-mission-bridge";
-import {withDatabase} from "./middlewares/withDatabase";
 import {Context, RootTypeResolvers, SubscriptionTypeResolvers, isError} from './types';
 import Database from '../db/Database';
 import config from '../config';
@@ -14,9 +14,13 @@ import * as userService from '../services/user-service';
 import * as teamService from '../services/team-service';
 import * as promoCodeService from '../services/promocode-service';
 import {AchievementEnum, UserTest} from "../db";
-import {mapUserFromMongoToGQLFormat} from "../services/mapper";
+import {mapUser} from "../services/mapper";
 import {UnknownError} from "./errors";
 import * as utils from '../services/utils';
+import * as usersResolvers from "./resolvers/usersResolvers";
+import * as teamsResolvers from './resolvers/teamsResolvers';
+import * as achievementsResolvers from './resolvers/achievementsResolvers';
+import {withSignValidation, withDatabase, withSignValidationAndDb} from "./middlewares";
 
 type TestQuery = {};
 type TestMutation = {};
@@ -28,110 +32,20 @@ type TestMutation = {};
  */
 export function createApolloServer(db: Database) {
   const Query: RootTypeResolvers<Query> = {
-    currentUser: withDatabase(db).createResolver(
-      async (root, args, context: Context) => {
-        const {user, receivedPromoCodes} = await userService.getCurrentUserInfo(db, 1);
-        if (!user || !receivedPromoCodes) {
-          throw new UnknownError();
-        }
-        return {
-          user: mapUserFromMongoToGQLFormat(user),
-          receivedPromoCodes
-        }
-      },
-    ),
-    userTeam: withDatabase(db).createResolver(
-      (root, args, context: Context) => {
-        return userService.getUserTeam(db, 1);
-      }
-    ),
-    user: withDatabase(db).createResolver(
-      async (root, args, context: Context) => {
-        const user = await userService.getUser(db, args.id);
-        if (!user) {
-          throw new UnknownError();
-        }
-        const userData = mapUserFromMongoToGQLFormat(user);
-        return userData;
-      }
-    ), // OK.
-    profileMeta: withDatabase(db).createResolver(
-      (root, args, context: Context) => {
-        return userService.getProfileMeta(db);
-      }
-    ), // OK.
-    mates: withDatabase(db).createResolver(
-      async (root, args, context: Context) => {
-        const {city, gamesIds} = args.searchFilter;
-        const mates = await teamService.searchMates(db, city, gamesIds, 1);
-        if (!(Array.isArray(mates) && mates.length)) {
-          throw new UnknownError();
-        }
-        return mates.map(x => mapUserFromMongoToGQLFormat(x));
-      }
-    ),
-    achievements: withDatabase(db).createResolver(
-       (root, args, context: Context) => {
-        return achievementService.getAchievements(db);
-      }
-    ), // OK.
+    currentUser: withSignValidationAndDb(db).createResolver(usersResolvers.getCurrentUser),
+    userTeam: withSignValidationAndDb(db).createResolver(usersResolvers.getUserTeam),
+    user: withSignValidationAndDb(db).createResolver(usersResolvers.getUser),
+    profileMeta: withSignValidationAndDb(db).createResolver(usersResolvers.getProfileMeta),
+    mates: withSignValidationAndDb(db).createResolver(teamsResolvers.searchMates),
+    achievements: withSignValidationAndDb(db).createResolver(achievementsResolvers.getAchievements),
   };
-  const Mutation: RootTypeResolvers<Mutation> = {
-    activateCheck: withDatabase(db).createResolver(
-      async (root, args, context: Context) => {
-        const check = args.check;
-        if (!utils.isString(check) || utils.isEmptyString(check)) {
-          throw new UnknownError();
-        }
-        const {error, ...rest} = await userService.activateCheck(args.check, db, 1);
-        const {message, activated, user, promoReceived} = rest;
-        if (error || !message || !activated || !user || !promoReceived) {
-          throw new UnknownError();
-        }
-        return {
-          message,
-          activated,
-          user: mapUserFromMongoToGQLFormat(user),
-          promoReceived
-        }
-      },
-    ),
-    saveProfile: withDatabase(db).createResolver(
-      async (root, args, context: Context) => {
-        const result = await userService.saveProfile(args.profile, db, 1);
-        if (isError(result)) {
-          throw new UnknownError();
-        }
-        return result;
-      },
-    ),
-    deleteProfile: withDatabase(db).createResolver(
-      (root, args, context: Context) => {
-        return userService.deleteProfile(db, 1);
-      }
-    ),
-    registerUser: withDatabase(db).createResolver(
-      async (root, args, context: Context) => {
-        const {user, newbiePromoReceived} = await userService.registerUser(db, 1, '');
-        const userData: GqlUser = mapUserFromMongoToGQLFormat(user);
-        return {user: userData, newbiePromoReceived};
-      }
-    ), // OK.
-    joinTargetUser: withDatabase(db).createResolver(
-      async (root, args, context: Context) => {
-        const result = await teamService.joinTargetUser(db, 1, 2);
-        if (isError(result)) {
-          throw new UnknownError();
-        }
-        return result;
-      }
-    ),
-    leaveTeam: withDatabase(db).createResolver(
-      async (root, args, context: Context) => {
-        const {data} = await teamService.leaveTeam(db, 1)
-        return data;
-      }
-    ),
+  const Mutation: RootTypeResolvers<TestMutation> = {
+    activateCheck: withSignValidationAndDb(db).createResolver(usersResolvers.activateCheck),
+    saveProfile: withSignValidationAndDb(db).createResolver(usersResolvers.saveProfile),
+    deleteProfile: withSignValidationAndDb(db).createResolver(usersResolvers.deleteProfile),
+    registerUser: withSignValidationAndDb(db).createResolver(usersResolvers.registerUser),
+    joinTargetUser: withSignValidationAndDb(db).createResolver(teamsResolvers.joinTargetUser),
+    leaveTeam: withSignValidationAndDb(db).createResolver(teamsResolvers.leaveTeam),
     // TODO возвращаемый тип должен быть UserPromoCode
     /*openPromoCode: withDatabase(db).createResolver(
       async (root, args, context: Context) => {
