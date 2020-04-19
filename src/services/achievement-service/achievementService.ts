8377193665d3e491a75f806bdbef3a237331294d
yungKillaGit <59793907+yungKillaGit@users.noreply.github.com>
2020-04-19 13:24:13 +0300

    add new functionality in achievement service

--- src/services/achievement-service/achievementService.ts
+++ src/services/achievement-service/achievementService.ts
@@ -1,13 +1,15 @@
-import Database, {Collection, IAchievement} from '../../db';
+import Database, {AchievementEnum, Collection, IAchievement, PromoCodeType} from '../../db';
+import {generatePromoCode} from "../promocode-service";
+import {getUserRanks} from "../user-service/utils";
 
-interface ICheckAchievementResult {
+export interface ICheckAchievementResult {
   addToReceivedRequired: boolean;
   levelUpgrade: boolean;
   pointsToAdd: number;
 }
 
 /**
- * Возвращает уровень относительно указанных очков.
+ * Возвращаем уровень относительно указанных очков.
  * @param points
  * @param currentPoints
  */
@@ -54,9 +56,96 @@ export const checkAchievement = (
 };
 
 /**
- * Получаем все достижения
+ * Выполняем получение достижения пользователем
+ * @param db
+ * @param achievementId
+ * @param userId
+ */
+export const processAchievement = async (db: Database, achievementId: AchievementEnum, userId: number) => {
+  const usersCollection = db.collection(Collection.Users);
+  let promoReceived = false;
+  const [achievements, ranks, foundUser] = await Promise.all([
+    db.collection(Collection.Achievements).find({}).toArray(),
+    db.collection(Collection.Ranks).find({}).toArray(),
+    usersCollection.findOne({id: userId}),
+  ]);
+
+  if (foundUser && achievements && ranks) {
+    const achievement = achievements.find(a => a.id === achievementId);
+
+    if (achievement) {
+      const alreadyReceived = foundUser.achievementsReceived.some(a => {
+        return a.id === achievement.id;
+      });
+
+      if (
+        !alreadyReceived
+        && achievement.rankPoints
+        && achievement.rankPoints.length > 0
+      ) {
+        promoReceived = true;
+        const promoCode = generatePromoCode(
+          PromoCodeType.Discount20VipFrom2Hours,
+        );
+        foundUser.points += achievement.rankPoints[0];
+        foundUser.achievementsProgress[achievement.id] = 1;
+        foundUser.achievementsReceived.push(achievement);
+        foundUser.promoCodes.push(promoCode);
+
+        const {rank, nextRank} = getUserRanks(foundUser.points, ranks);
+        if (rank) {
+          foundUser.rank = rank;
+        }
+        if (nextRank) {
+          foundUser.nextRank = nextRank;
+        }
+
+        await usersCollection.updateOne(
+          {id: userId},
+          {
+            $set: {
+              rank: foundUser.rank,
+              nextRank: foundUser.nextRank,
+              points: foundUser.points,
+              [`achievementsProgress.${achievement.id}`]: 1,
+            },
+            $push: {
+              achievementsReceived: achievement,
+              promoCodes: promoCode,
+            },
+          },
+        )
+      }
+    }
+  }
+  return {
+    user: !foundUser ? null : foundUser,
+    promoReceived,
+  }
+};
+
+/**
+ * Возвращаем все достижения
  * @param db
  */
-export const getAchievements = async (db: Database) => {
-  return await db.collection(Collection.Achievements).find().toArray()
+export const getAchievements = (db: Database) => {
+  return db.collection(Collection.Achievements).find().toArray()
+};
+
+/**
+ * Репост
+ * @param db
+ * @param userId
+ */
+export const processPostSharing = (db: Database, userId: number) => {
+  return processAchievement(db, AchievementEnum.RichSoul, userId);
+};
+
+/**
+ * Подписка на группу
+ * @param db
+ * @param userId
+ */
+export const processGroupSubscription = (db: Database, userId: number) => {
+  return processAchievement(db, AchievementEnum.Correspondent, userId);
 };
